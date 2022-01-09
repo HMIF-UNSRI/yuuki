4a40070b5a7cab5d2ebac7c8788c1a3593d1929b
Arya Yunanta <aryayun90@gmail.com>
2022-01-09 20:05:31 +0700

    Add shortener api

--- app/shortener/repository/shortener_repository.go
+++ app/shortener/repository/shortener_repository.go
@@ -1,1 +1,56 @@
 package repository
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"yuuki/domain"
+	"yuuki/pkg/helper"
+)
+
+type shortenerRepository struct {
+	db *sql.DB
+}
+
+func NewShortenerRepository(db *sql.DB) *shortenerRepository {
+	return &shortenerRepository{db: db}
+}
+
+func (repository *shortenerRepository) Create(ctx context.Context, shortener domain.Shortener) domain.Shortener {
+	statement := "INSERT INTO shorteners(real_url, slug) VALUE (?, ?);"
+	result, err := repository.db.ExecContext(ctx, statement, shortener.RealURL, shortener.Slug)
+	helper.PanicIfErr(err)
+
+	id, err := result.LastInsertId()
+	helper.PanicIfErr(err)
+
+	shortener.ID = int(id)
+	return shortener
+}
+
+func (repository *shortenerRepository) Delete(ctx context.Context, id int) domain.Shortener {
+	panic("implement me")
+}
+
+func (repository *shortenerRepository) FindBy(ctx context.Context, statement string, args []interface{}) (domain.Shortener, error) {
+	rows, err := repository.db.QueryContext(ctx, statement, args...)
+	helper.PanicIfErr(err)
+	defer rows.Close()
+
+	shortener := domain.Shortener{}
+	if rows.Next() {
+		helper.PanicIfErr(rows.Scan(&shortener.ID, &shortener.RealURL, &shortener.Slug))
+		return domain.Shortener{
+			ID:      shortener.ID,
+			RealURL: shortener.RealURL,
+			Slug:    shortener.Slug,
+		}, nil
+	} else {
+		return shortener, errors.New("shortener is not found")
+	}
+}
+
+func (repository *shortenerRepository) UpdateView(ctx context.Context, id int) {
+	statement := "UPDATE shorteners SET views=views + 1, last_viewed=CURRENT_TIMESTAMP WHERE id = ?;"
+	repository.db.ExecContext(ctx, statement, id)
+}
--- main.go
+++ main.go
@@ -9,10 +9,16 @@ import (
 	_categoryHandler "yuuki/app/category/delivery/http"
 	_categoryRepo "yuuki/app/category/repository"
 	_categoryUsecase "yuuki/app/category/usecase"
+
 	"yuuki/app/mariadb"
+	_shortenerHandler "yuuki/app/shortener/delivery/http"
+	_shortenerRepo "yuuki/app/shortener/repository"
+	_shortenerUsecase "yuuki/app/shortener/usecase"
+
 	_uploadHandler "yuuki/app/upload/delivery/http"
 	_uploadRepo "yuuki/app/upload/repository"
 	_uploadUsecase "yuuki/app/upload/usecase"
+
 	"yuuki/middleware"
 	"yuuki/pkg/config"
 	"yuuki/pkg/exception"
@@ -30,6 +36,9 @@ func main() {
 	uploadRepository := _uploadRepo.NewUploadRepository(database)
 	uploadUsecase := _uploadUsecase.NewUploadUsecase(uploadRepository)
 
+	shortenerRepository := _shortenerRepo.NewShortenerRepository(database)
+	shortenerUsecase := _shortenerUsecase.NewShortenerUsecase(shortenerRepository, validate)
+
 	logger, err := zap.NewProduction()
 	helper.PanicIfErr(err)
 	defer logger.Sync()
@@ -39,8 +48,9 @@ func main() {
 	router := httprouter.New()
 	router.PanicHandler = exception.ErrorHandler
 
-	_categoryHandler.RegisterProductHandler(router, categoryUsecase)
+	_categoryHandler.RegisterCategoryHandler(router, categoryUsecase)
 	_uploadHandler.RegisterUploadHandler(router, uploadUsecase)
+	_shortenerHandler.RegisterShortenerHandler(router, shortenerUsecase)
 
 	sugar.Infow("listening and serving http on :8080")
 	sugar.Fatal(http.ListenAndServe(":8080", &middleware.LogMiddleware{Handler: router, Logger: sugar}))
